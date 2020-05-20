a9b41205b4c67a2b80f5989c51e1220783a9b719
hay <hehee.hope@gmail.com>
2020-05-20 16:26:59 +0900

    S02P31A107-57 | back | chat | 채팅방 목록 불러오기

---
+++ back/hal-back/src/main/java/com/hal/model/service/RoomServiceImp.java
@@ -0,0 +1,35 @@
+package com.hal.model.service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hal.model.dao.RoomRepository;
+import com.hal.model.dto.Room;
+import com.hal.model.dto.User;
+
+@Service
+public class RoomServiceImp implements RoomService {
+
+	@Autowired
+	private RoomRepository roomRepository;
+	
+	public List<Room> findRoomListById(User user){
+		List<Room> roomList = new LinkedList<>();
+		List<Room> tmpList = new LinkedList<>();
+		
+		roomList = roomRepository.findBySender(user); //user가 Sender로 받아짐
+		tmpList = roomRepository.findByReceiver(user); //user가 Receiver로 받아짐
+		for(int i=0; i<tmpList.size(); i++) {
+			Room room = tmpList.get(i);
+			User receiver = room.getSender();
+			room.setReceiver(receiver);
+			room.setSender(user);
+			roomList.add(room);
+		}
+		
+		return roomList;
+	}
+}
