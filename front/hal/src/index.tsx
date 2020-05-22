afff135b7b1940660c11ab6ae9761f6449dd252d
godhpjh <godhpjh123@naver.com>
2020-05-22 12:41:55 +0900

    S02P31A107-2 | front | 환경설정 | 타입스크립트 react 환경설정microsoft/typesript

--- front/hal/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import registerServiceWorker from './ServiceWorker'
-import './index.css'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
