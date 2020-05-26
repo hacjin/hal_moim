2a1a268ba616048841496c5e70a3acaaf1fda742
최현정 <tian1785@naver.com>
2020-05-26 17:16:08 +0900

    S02P31A107-55 | front | chat | develop merge

--- front/hal/src/routes/tabs/chat.tsx
+++ front/hal/src/routes/tabs/chat.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 // import ChatRoomList from '../containers/ChatRoomList'
 // import '../Chat.css'
-import Chat from '../components/Chat'
+import Chat from '../../components/Chat'
 
 const chat = () => {
   return <div>이 페이지는 채팅 페이지 입니다.
