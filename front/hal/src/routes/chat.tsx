5cc09524481b7a80dab7b802a9c6b6b0b44661d8
최현정 <tian1785@naver.com>
2020-05-26 17:11:09 +0900

    S02P31A107-55 | front | chat | react-chat-window 적용

--- front/hal/src/routes/chat.tsx
+++ front/hal/src/routes/chat.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 // import ChatRoomList from '../containers/ChatRoomList'
-import '../Chat.css'
+// import '../Chat.css'
+import Chat from '../components/Chat'
 
 const chat = () => {
   return <div>이 페이지는 채팅 페이지 입니다.
 
     {/* <ChatRoomList></ChatRoomList> */}
+    <Chat/>
   </div>
 }
 
