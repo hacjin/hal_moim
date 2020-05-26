8314102203538d843add31e74dcebc0e62574cbb
hay <hehee.hope@gmail.com>
2020-05-26 16:20:34 +0900

    S02P31A107-47 | front | chat | 채팅 목록 리스트 ui 지움

--- front/hal/src/routes/chat.tsx
+++ front/hal/src/routes/chat.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ChatRoomList from '../containers/ChatRoomList'
+// import ChatRoomList from '../containers/ChatRoomList'
 import '../Chat.css'
 
 const chat = () => {
   return <div>이 페이지는 채팅 페이지 입니다.
 
-    <ChatRoomList></ChatRoomList>
+    {/* <ChatRoomList></ChatRoomList> */}
   </div>
 }
 
