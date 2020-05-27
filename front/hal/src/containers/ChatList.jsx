59511f88be23d3dffdd245178fc07e39705f0d69
hay <hehee.hope@gmail.com>
2020-05-27 15:13:31 +0900

    S02P31A107-47 | front | chat | API 통신 채팅방목록

--- front/hal/src/containers/ChatList.jsx
+++ front/hal/src/containers/ChatList.jsx
@@ -5,12 +5,14 @@ import { ListView, ListViewHeader, ListViewFooter } from '@progress/kendo-react-
 import { Avatar } from '@progress/kendo-react-layout';
 import '../styles/all.css'
 import '../styles/bootstrap.min.css'
+import API from '../util/API'
 
 import contacts from './contacts.json';
 
 
 const MyItemRender = props => {
     let item = props.dataItem;
+    console.log("item",item)
     return (
         <div className='row p-2 border-bottom align-middle' style={{ margin: 0}}>
             <div className='col-2'>
@@ -19,13 +21,13 @@ const MyItemRender = props => {
                 </Avatar>
             </div>
             <div className='col-6'>
-                <h2 style={{fontSize: 14, color: '#454545', marginBottom: 0}} className="text-uppercase">{item.name}</h2>
-                <div style={{fontSize: 12, color: "#a0a0a0"}}>{item.email}</div>
+                <h2 style={{fontSize: 14, color: '#454545', marginBottom: 0}} className="text-uppercase">{item.receiver}</h2>
+                <div style={{fontSize: 12, color: "#a0a0a0"}}></div>
             </div>
             <div className='col-4'>
                 <div className='k-chip k-chip-filled'>
                     <div className='k-chip-content'>
-                        {item.messages} new
+                        new
                     </div>
                 </div>
             </div>
@@ -34,11 +36,43 @@ const MyItemRender = props => {
 }
 
 class ChatList extends React.Component {
-    render() {
+    constructor(){
+        super();
+        this.state = {
+            receiverData:[],
+        }
+    }
+
+    async componentDidMount() {
+        // Load async data.
+        let userData = await API.get('chat/findRoomListById', {
+          params: {
+            uid: 1
+          }
+        });
+
+        console.log(userData.data.data[0].receiver.name)
+
+        // Parse the results for ease of use.
+  
+
+        // Update state with new data and re-render our component.
+        // const name = `${userData.name.first} ${userData.name.last}`;
+        // const avatar = userData.picture.large;
+        // const email = userData.email;
+
+        this.setState({
+          ...this.state, ...{
+            receiverData : userData.data.data
+          }
+        });
+    }
+
+    render() {  
         return (
             <div>
                 <ListView
-                    data={contacts} //contacts : json데이터
+                    data={this.state.receiverData} //contacts : json데이터
                     item={MyItemRender}
                     style={{ width: "100%" }}
                 />
