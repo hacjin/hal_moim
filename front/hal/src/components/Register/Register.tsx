451ca3a06d4cc236143bbdffe76bdf6a14352863
godhpjh <godhpjh123@naver.com>
2020-06-04 01:54:42 +0900

    S02P31A107-18 | front | regist | 회원가입 faceimage 테스트

--- front/hal/src/components/Register/Register.tsx
+++ front/hal/src/components/Register/Register.tsx
@@ -15,7 +15,7 @@ const Register = ( props:any ) => {
   const [gender, setGender] = useState('');
   const [birth, setBirth] = useState('');
   const [addr, setAddr] = useState('');
-  const [myImg, setMyImg] = useState('');
+  const [myImg, setMyImg] = useState(new Blob());
   const [latitude, setLatitude] = useState('');
   const [longitude, setlongitude] = useState('');
   const [open, setOpen] = useState(false);
@@ -103,7 +103,12 @@ const Register = ( props:any ) => {
     setGender(e.target.value);
   }
   const handleMyImg = (e:any) => {
-    setMyImg(e.target.files[0]);
+    //setMyImg(e.target.files[0]);
+    const bufferToBlob = new Blob([ 
+      JSON.stringify({e})
+    ], { type: 'application.json'});
+
+    setMyImg(bufferToBlob);
   }
   const handleOpen = () => {
     setOpen(true);
@@ -126,7 +131,7 @@ const Register = ( props:any ) => {
       formdata.append('addr',addr);
       formdata.append('latitude',latitude);
       formdata.append('longitude',longitude);
-      formdata.append('myImg',myImg);
+      formdata.append('myImg',myImg, phone+'.png');
 
       doRegist(formdata);
     }
@@ -230,7 +235,7 @@ const Register = ( props:any ) => {
             <Button id="addr" variant="contained" color="secondary" 
               size="large" fullWidth
               onClick={handleOpen} >사진등록</Button>
-            <Registface open={open} setOpen={setOpen}/>
+            <Registface open={open} setOpen={setOpen} imgValue={handleMyImg}/>
           </Grid>
         </Grid>
         
--- front/hal/src/components/Register/Registface.jsx
+++ front/hal/src/components/Register/Registface.jsx
@@ -3,6 +3,9 @@ import React, {useState} from 'react'
 import { makeStyles,  Dialog, DialogTitle, DialogContent, Button, } from '@material-ui/core'
 import Webcam from 'react-webcam';
 
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
 const useStyles = makeStyles((theme) => ({
     dialog: {
       textAlign: 'center',
@@ -12,34 +15,65 @@ const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(1),
       },
       '& > div': {
-        width: '80%',
+        width: '100%',
       },
     },
-    input: {
-      display: 'none',
-    },
-    preview: {
-      margin: '0px auto',
-      backgroundColor: '#efefef',
-      width: '300px',
-      height: '200px',
+    closeButton: {
+      position: 'absolute',
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.grey[500],
     },
 }))
 
 const Registface = ( props ) => {
     const classes = useStyles();
-    const [ref, setRef] = useState(React.createRef());
+    const ref = React.useRef();
 
-    const handleClose = () => {
-        props.setOpen(false)
-    }
+    const [url, setUrl] = useState();
+    const [retake, setRetake] = useState(false);
 
     const handleCapture = () => {
-      const imgsrc = props.webcam.getScreenshot();
+      const imgsrc = ref.current.getScreenshot();
+      
+      captureImage(imgsrc, (url) => {
+        setUrl(url);
+        setRetake(true);
+      });
+
+    }
+
+    const captureImage = (imageBase64, cb) => {
+      var img = new Image();
+      img.src = imageBase64;
+      img.onload = () => {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        var ctx = canvas.getContext("2d");
+        ctx.setTransform(1, 0, 0, 1, img.width , img.height );
+        //ctx.rotate(rotation * (Math.PI / 180));
+        ctx.drawImage(img, -img.width , -img.height );
+        cb(canvas.toDataURL("image/png"))
+      }
+    };
+
+    const handleRetake = (e) => {
+      setRetake(false);
+    }
+    const handleSave = (e) => {
+      const buffer = Buffer.from(url, 'base64');
+
+      props.imgValue(buffer); // 부모 컴포넌트로 값을 넘긴다.
+      handleClose(); // Dialog 닫기
+    }
+
+    const handleClose = () => {
+      props.setOpen(false);
     }
 
     const videoConstraints = {
-      width: 300,
+      width:  300,
       height: 300,
       facingMode: "user"
     };
@@ -50,38 +84,39 @@ const Registface = ( props ) => {
                 //
                 className={classes.dialog}
                 open={props.open}
-                onClose={handleClose}
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
                 fullWidth={true}
-            >
-                <DialogTitle id="alert-dialog-title">사진 캡쳐</DialogTitle>
-                <DialogContent>
+            >   
+                <IconButton aria-label="close" className={classes.closeButton} onClick={handleClose}>
+                  <CloseIcon />
+                </IconButton>
+                <DialogTitle id="alert-dialog-title">얼굴 등록</DialogTitle>
+                <DialogContent dividers>
                 <div className={classes.root}>
-                    {/* <div className={classes.preview}>{imgBase64 === '' ? '' : <img className={classes.preview} src={imgBase64} alt="미리보기" />}</div> */}
-                    <input
-                    //
-                    accept="image/*"
-                    className={classes.input}
-                    id="contained-button-file"
-                    multiple
-                    type="file"
-                    //onChange={handleImg}
-                    />
-                    <label htmlFor="contained-button-file">
-                        <Button variant="contained" color="primary" component="span" onClick={handleCapture}>
-                            저장하기
-                        </Button>
-                    </label>
-
-                    <Webcam 
-                      audio={false}
-                      height={300}
-                      width='100%'
-                      //ref={setRef(ref)}
-                      videoConstraints={videoConstraints}
-                    />
-                    <Button variant="contained" color="secondary" component="span" onClick={handleClose}>돌아가기</Button>
+                    {retake === false ?
+                    <div>
+                      <Webcam 
+                        audio={false}
+                        height={300}
+                        width='100%'
+                        ref={ref}
+                        screenshotFormat="image/png"
+                        videoConstraints={videoConstraints}
+                      />
+                      <Button variant="contained" color="primary" component="span" onClick={handleCapture}>
+                        캡쳐하기
+                      </Button>
+                    </div>
+                    : 
+                    <div>
+                      <img src={url} alt="dd"></img>
+                      <canvas id="canvas" width='100%' style={{display: 'none'}}></canvas>
+                      <br/><br/><br/>
+                      <Button variant="contained" color="primary" onClick={handleRetake}>다시찍기</Button> &nbsp;&nbsp;&nbsp;
+                      <Button variant="contained" color="secondary" onClick={handleSave}>저장하기</Button>
+                    </div>
+                    }
                 </div>
                 </DialogContent>
             </Dialog>
