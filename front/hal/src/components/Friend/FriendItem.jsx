00f7c621cf6fec55024294eddf32034008021769
hay <hehee.hope@gmail.com>
2020-06-03 16:00:07 +0900

    S02P31A107-24 | front | friend | 친구리스트 ui

--- front/hal/src/components/Friend/FriendItem.jsx
+++ front/hal/src/components/Friend/FriendItem.jsx
@@ -1,137 +1,147 @@
-import React from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Tooltip from '@material-ui/core/Tooltip';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import CallMade from '@material-ui/icons/CallMade';
+import React,{useState} from 'react';
+import cx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import TextInfoContent from '@mui-treasury/components/content/textInfo';
+import { useBlogTextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/blog';
+import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over';
+import API from "../../apis/api"
 
-import { Row, Column, Item } from '@mui-treasury/components/flex'
-import { useSizedIconButtonStyles } from '@mui-treasury/styles/iconButton/sized';
-
-const StyledTooltip = withStyles({
-  tooltip: {
-    marginTop: '0.2rem',
-    backgroundColor: 'rgba(0,0,0,0.72)',
-    color: '#fff',
+const useStyles = makeStyles(({ breakpoints, spacing }) => ({
+  root: {
+    margin: 'auto',
+    // borderRadius: spacing(2), // 16px
+    transition: '0.3s',
+    // boxShadow: '0px 14px 80px rgba(34, 35, 58, 0.2)',
+    position: 'relative',
+    // maxWidth: 500,
+    marginLeft: 'auto',
+    overflow: 'initial',
+    background: '#ffffff',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingBottom: spacing(2),
+    [breakpoints.up('xs')]: {
+      flexDirection: 'row',
+      paddingTop: spacing(2),
+    },
   },
-})(Tooltip);
-
-const useBasicProfileStyles = makeStyles(({ palette }) => ({
-  avatar: {
-    borderRadius: 8,
-    backgroundColor: '#495869'
+  media: {
+    width: '100%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    // marginTop: spacing(-3),
+    height: 0,
+    paddingBottom: '48%',
+    borderRadius: spacing(2),
+    backgroundColor: '#fff',
+    position: 'relative',
+    [breakpoints.up('md')]: {
+      width: '100%',
+      marginLeft: spacing(-3),
+      marginTop: 0,
+      transform: 'translateX(-8px)',
+    },
+    '&:after': {
+      content: '" "',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      // backgroundImage: 'linear-gradient(147deg, #fe8a39 0%, #fd3838 74%)',
+      borderRadius: spacing(2), // 16
+      opacity: 0.5,
+    },
   },
-  overline: {
-    fontSize: 10,
-    textTransform: 'uppercase',
-    letterSpacing: 1,
-    color: '#8D9CAD',
+  content: {
+    padding: 24,
   },
-  name: {
-    fontSize: 14,
-    fontWeight: 500,
-    color: '#495869',
-  }
-}))
+  cta: {
+    marginTop: 24,
+    textTransform: 'initial',
+  },
+}));
 
-const BasicProfile = props => {
-  const styles = useBasicProfileStyles()
-  return (
-    <Row gutter={'inherit'} {...props}>
-      <Avatar className={styles.avatar}>S</Avatar>
-      <Item position={'middle'} pl={{ xs: 1, sm: 1.5 }}>
-        <Typography className={styles.overline}>
-          CREATOR
-        </Typography>
-        <Typography className={styles.name}>
-          siriwatknp
-        </Typography>
-      </Item>
-    </Row>
-  )
+const FriendItem = (props) => {
+
+  const item = props.dataItem
+  const styles = useStyles();
+  const {
+    button: buttonStyles,
+    ...contentStyles
+  } = useBlogTextInfoContentStyles();
+  const shadowStyles = useOverShadowStyles();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  console.log(props.dataItem)
+
+  async function _onFormSubmit(){
+
+    console.log("onform", item)
+    //이친구와 방이 있나 확인(있으면 방 번호 리턴, 없으면 방 만들고 방번호 리턴)
+    const roomId = await API.get('chat/addRoom',{
+        params:{
+            senderId: 1, //세션
+            receiverId: item.uid
+        }
+    })
+
+    //채팅방 메시지 불러오기
+    let totalChatData = await API.get('chat/findChatListById', {
+        params: {
+          rid : roomId.data.data
+        }
+    })
+    var roomMessageList = []
+    
+    totalChatData.data.data.forEach(function(item, index, array){
+        var replytext 
+        if(item.type ==='text'){
+          replytext = {'text':item.message}
+        }else{
+          replytext = {'emoji':item.message}
+        }
+        roomMessageList.push({
+            author: item.sender.uid==1?'me':'them',
+            type: item.type,
+            data: replytext
+            })
+    })
+
+    
+    setIsOpen(true);
+    props.openChatWindow(true,roomId.data.data,item,roomMessageList);
 }
 
-const useCardHeaderStyles = makeStyles(() => ({
-  root: { paddingBottom: 0 },
-  title: {
-    fontSize: '1.25rem',
-    color: '#122740',
-  },
-  subheader: {
-    fontSize: '0.875rem',
-    color: '#495869',
-  },
-}))
 
-const CardHeader = props => {
-  const styles = useCardHeaderStyles()
-  const iconBtnStyles = useSizedIconButtonStyles({ padding: 8, childSize: 20 });
   return (
-    <Row {...props}>
-      <Item position={'middle'}>
-        <Typography className={styles.title}>
-          <b>Firebase</b>
-        </Typography>
-        <Typography className={styles.subheader}>
-          Similar to firebase theme
-        </Typography>
-      </Item>
-      <Item position={'right'} mr={-0.5}>
-        <StyledTooltip title={'See details'}>
-          <IconButton classes={iconBtnStyles}>
-            <CallMade />
-          </IconButton>
-        </StyledTooltip>
-      </Item>
-    </Row>
-  )
-}
-
-const useStyles = makeStyles(() => ({
-  card: {
-    border: '2px solid',
-    borderColor: '#E7EDF3',
-    borderRadius: 16,
-    transition: '0.4s',
-    '&:hover': {
-      borderColor: '#5B9FED',
-    }
-  }
-}))
-
-const FriendItem = () => {
-  const styles = useStyles()
-  return (
-    <Grid container spacing={4} justify={'center'}>
-      <Grid item xs={12} sm={4} md={3}>
-        <Column className={styles.card} gutter={{ xs: 1, sm: 1.5, lg: 2 }}>
-          <CardHeader />
-          <Item>
-            <Box minHeight={200} bgcolor={'#F4F7FA'} borderRadius={8}>
-
-            </Box>
-          </Item>
-          <BasicProfile />
-        </Column>
-      </Grid>
-      <Grid item xs={12} sm={8} lg={7}>
-        <Row className={styles.card} gutter={{ xs: 1, sm: 1.5, lg: 2 }}>
-          <Item grow>
-            <Box minHeight={200} bgcolor={'#F4F7FA'} borderRadius={8}>
-
-            </Box>
-          </Item>
-          <Column gutter={'inherit'}>
-            <CardHeader />
-            <BasicProfile position={'bottom'} />
-          </Column>
-        </Row>
-      </Grid>
-    </Grid>
-  )
+    <Card className={cx(styles.root, shadowStyles.root)}>
+      <CardMedia
+        className={styles.media}
+        image={
+          // item.profileImg
+          "../../public/logo.jpg"
+        }
+      />
+      <CardContent>
+        <TextInfoContent
+          classes={contentStyles}
+          // overline={'28 MAR 2019'}
+          heading={item.name}
+          body={
+            'Git is a distributed version control system. Every dev has a working copy of the code and...'
+          }
+        />
+        <Button variant="contained" color="primary" fullWidth="true" onClick={_onFormSubmit}>메세지 보내기</Button>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default FriendItem;
--- front/hal/src/components/Friend/flex/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Row } from './Row'
-export { default as Column } from './Column'
-export { default as Item } from './Item'
