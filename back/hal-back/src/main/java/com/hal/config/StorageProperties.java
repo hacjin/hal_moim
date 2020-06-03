2352b29508c545cc0bf8d49b84416ac4e24358a5
godhpjh <godhpjh123@naver.com>
2020-06-03 14:46:14 +0900

    S02P31A107-18 | front | main | 회원가입 및 로그인

---
+++ back/hal-back/src/main/java/com/hal/config/StorageProperties.java
@@ -0,0 +1,18 @@
+//package com.hal.config;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+////application.properties에 선언한 file.upload-dir에 접근함.
+//@ConfigurationProperties
+//public class StorageProperties {
+//	
+//	private String uploadDir;
+//
+//    public String getUploadDir() {
+//        return uploadDir;
+//    }
+//
+//    public void setUploadDir(String uploadDir) {
+//        this.uploadDir = uploadDir;
+//    }
+//}
