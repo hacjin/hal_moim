3255af94df6283343e4b5c97273ba4b825f2b6f3
hay <hehee.hope@gmail.com>
2020-06-03 15:06:18 +0900

    S02P31A107-24 | front | friend | 친구리스트 ui

--- front/hal/src/components/Friend/FriendItem.jsx
+++ front/hal/src/components/Friend/FriendItem.jsx
@@ -1,87 +1,137 @@
-import React, {useState} from 'react';
-import { Avatar } from '@progress/kendo-react-layout';
-import API from "../../apis/api"
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Tooltip from '@material-ui/core/Tooltip';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import CallMade from '@material-ui/icons/CallMade';
 
-const FriendItem = props => {
+import { Row, Column, Item } from '@mui-treasury/components/flex'
+import { useSizedIconButtonStyles } from '@mui-treasury/styles/iconButton/sized';
 
-    let item = props.dataItem;
-    const [isOpen, setIsOpen] = useState(false);
+const StyledTooltip = withStyles({
+  tooltip: {
+    marginTop: '0.2rem',
+    backgroundColor: 'rgba(0,0,0,0.72)',
+    color: '#fff',
+  },
+})(Tooltip);
 
+const useBasicProfileStyles = makeStyles(({ palette }) => ({
+  avatar: {
+    borderRadius: 8,
+    backgroundColor: '#495869'
+  },
+  overline: {
+    fontSize: 10,
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+    color: '#8D9CAD',
+  },
+  name: {
+    fontSize: 14,
+    fontWeight: 500,
+    color: '#495869',
+  }
+}))
 
-    async function _onFormSubmit(){
-
-        console.log("onform", item)
-        //이친구와 방이 있나 확인(있으면 방 번호 리턴, 없으면 방 만들고 방번호 리턴)
-        const roomId = await API.get('chat/addRoom',{
-            params:{
-                senderId: 1, //세션
-                receiverId: item.uid
-            }
-        })
-
-        //채팅방 메시지 불러오기
-        let totalChatData = await API.get('chat/findChatListById', {
-            params: {
-              rid : roomId.data.data
-            }
-        })
-        var roomMessageList = []
-        
-        totalChatData.data.data.forEach(function(item, index, array){
-            var replytext 
-            if(item.type ==='text'){
-              replytext = {'text':item.message}
-            }else{
-              replytext = {'emoji':item.message}
-            }
-            roomMessageList.push({
-                author: item.sender.uid==1?'me':'them',
-                type: item.type,
-                data: replytext
-                })
-        })
-
-        
-        setIsOpen(true);
-        props.openChatWindow(true,roomId.data.data,item,roomMessageList);
-    }
-
-
-    const classList = [
-    'chat-launcher',
-    'row',
-    'p-2',
-    'border-bottom',
-    'align-middle',
-    (isOpen ? 'opened' : ''),
-   ];
-
-
-    return (
-        
-            <div className={classList.join(' ')} style={{ margin: 0}} onClick={_onFormSubmit}>
-
-                <div className='col-2'>
-                    <Avatar shape='circle' type='img'>
-                        <img src={`https://gist.github.com/simonssspirit/0db46d4292ea8e335eb18544718e2624/raw/2a595679acdb061105c80bd5eeeef58bb90aa5af/${item.profileImg}-round-40x40.png`} />
-                    </Avatar>
-                </div>
-                <div className='col-6'>
-                    <h2 style={{fontSize: 14, color: '#454545', marginBottom: 0}} className="text-uppercase">{item.name}</h2>
-                    <div style={{fontSize: 12, color: "#a0a0a0"}}>{}</div>
-                </div>
-                <div className='col-4'>
-                    <div className='k-chip k-chip-filled'>
-                        <div className='k-chip-content'>
-                            {} new
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-    )
-    
-
+const BasicProfile = props => {
+  const styles = useBasicProfileStyles()
+  return (
+    <Row gutter={'inherit'} {...props}>
+      <Avatar className={styles.avatar}>S</Avatar>
+      <Item position={'middle'} pl={{ xs: 1, sm: 1.5 }}>
+        <Typography className={styles.overline}>
+          CREATOR
+        </Typography>
+        <Typography className={styles.name}>
+          siriwatknp
+        </Typography>
+      </Item>
+    </Row>
+  )
 }
-export default FriendItem
+
+const useCardHeaderStyles = makeStyles(() => ({
+  root: { paddingBottom: 0 },
+  title: {
+    fontSize: '1.25rem',
+    color: '#122740',
+  },
+  subheader: {
+    fontSize: '0.875rem',
+    color: '#495869',
+  },
+}))
+
+const CardHeader = props => {
+  const styles = useCardHeaderStyles()
+  const iconBtnStyles = useSizedIconButtonStyles({ padding: 8, childSize: 20 });
+  return (
+    <Row {...props}>
+      <Item position={'middle'}>
+        <Typography className={styles.title}>
+          <b>Firebase</b>
+        </Typography>
+        <Typography className={styles.subheader}>
+          Similar to firebase theme
+        </Typography>
+      </Item>
+      <Item position={'right'} mr={-0.5}>
+        <StyledTooltip title={'See details'}>
+          <IconButton classes={iconBtnStyles}>
+            <CallMade />
+          </IconButton>
+        </StyledTooltip>
+      </Item>
+    </Row>
+  )
+}
+
+const useStyles = makeStyles(() => ({
+  card: {
+    border: '2px solid',
+    borderColor: '#E7EDF3',
+    borderRadius: 16,
+    transition: '0.4s',
+    '&:hover': {
+      borderColor: '#5B9FED',
+    }
+  }
+}))
+
+const FriendItem = () => {
+  const styles = useStyles()
+  return (
+    <Grid container spacing={4} justify={'center'}>
+      <Grid item xs={12} sm={4} md={3}>
+        <Column className={styles.card} gutter={{ xs: 1, sm: 1.5, lg: 2 }}>
+          <CardHeader />
+          <Item>
+            <Box minHeight={200} bgcolor={'#F4F7FA'} borderRadius={8}>
+
+            </Box>
+          </Item>
+          <BasicProfile />
+        </Column>
+      </Grid>
+      <Grid item xs={12} sm={8} lg={7}>
+        <Row className={styles.card} gutter={{ xs: 1, sm: 1.5, lg: 2 }}>
+          <Item grow>
+            <Box minHeight={200} bgcolor={'#F4F7FA'} borderRadius={8}>
+
+            </Box>
+          </Item>
+          <Column gutter={'inherit'}>
+            <CardHeader />
+            <BasicProfile position={'bottom'} />
+          </Column>
+        </Row>
+      </Grid>
+    </Grid>
+  )
+};
+
+export default FriendItem;
---
+++ front/hal/src/components/Friend/flex/Column.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import cx from 'clsx';
+import Item, { ItemProps } from './Item';
+import { Provider, useFlexCtx, Gutter } from './core';
+
+export type ColumnProps = ItemProps & {
+  gutter?: Gutter;
+  children: React.ReactNode | React.ReactElement | React.ReactElement[];
+};
+
+const Column = ({ className, children, gutter, ...props }: ColumnProps) => {
+  const inheritGutter = useFlexCtx() ? gutter || 'inherit' : gutter;
+  return (
+    <Item
+      className={cx('FlexColumn', className)}
+      display={'flex'}
+      flexDirection={'column'}
+      {...(inheritGutter !== 'inherit' && { p: inheritGutter })}
+      {...props}
+    >
+      <Provider flexDirection={'column'} gutter={inheritGutter}>
+        {children}
+      </Provider>
+    </Item>
+  );
+};
+
+export default Column;
