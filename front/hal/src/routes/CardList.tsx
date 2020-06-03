2facbae30d2f1bab9f8c10d81c8ca3bd8c003b51
hacjin <macjin102@naver.com>
2020-06-03 14:35:14 +0900

    S02P31A107-74|front|back|Moim_Tab_feature_success

--- front/hal/src/routes/CardList.tsx
+++ front/hal/src/routes/CardList.tsx
@@ -123,80 +123,84 @@ const CardList = ({ data, classes, setUpdate }: CardProps) => {
   const time = data.time.split(/[. : T -]/)
   return (
     <>
-      <Card elevation={1} className={classes.card}>
-        <div className={classes.cardWrapper}>
-          <div className={classes.front}>
-            <CardMedia className={classes.media} component={'img'} image={'https://image.freepik.com/free-photo/river-foggy-mountains-landscape_1204-511.jpg'} />
-            <CardContent className={classes.content}>
-              <Typography className={classes.heading} variant={'h6'} gutterBottom>
-                {data.title}
-              </Typography>
-              <Typography className={classes.subheading} variant={'caption'}>
-                장소 : {data.location}
+      {data.state ? (
+        <Card elevation={1} className={classes.card}>
+          <div className={classes.cardWrapper}>
+            <div className={classes.front}>
+              <CardMedia className={classes.media} component={'img'} image={'https://image.freepik.com/free-photo/river-foggy-mountains-landscape_1204-511.jpg'} />
+              <CardContent className={classes.content}>
+                <Typography className={classes.heading} variant={'h6'} gutterBottom>
+                  {data.title}
+                </Typography>
+                <Typography className={classes.subheading} variant={'caption'}>
+                  장소 : {data.location}
+                  <br />
+                </Typography>
+                <Typography className={classes.subheading} variant={'caption'}>
+                  시간 : {time[0]}년 {time[1]}월 {time[2]}일 {time[3]}시:{time[4]}분까지
+                </Typography>
+                <Typography className={classes.heading} variant={'h6'}>
+                  설명 : {data.coment}
+                </Typography>
+              </CardContent>
+            </div>
+            <div className={classes.back}>
+              <CardMedia className={classes.media} component={'img'} image={'https://image.freepik.com/free-photo/river-foggy-mountains-landscape_1204-511.jpg'} />
+              <CardContent className={classes.content}>
+                <Typography className={classes.heading} variant={'h6'} gutterBottom>
+                  주최자 : {data.host.name}
+                </Typography>
+                <Typography className={classes.subheading} variant={'caption'}>
+                  생년월일 : {data.host.birth}
+                </Typography>
                 <br />
-              </Typography>
-              <Typography className={classes.subheading} variant={'caption'}>
-                시간 : {time[0]}년 {time[1]}월 {time[2]}일 {time[3]}시:{time[4]}분까지
-              </Typography>
-              <Typography className={classes.heading} variant={'h6'}>
-                설명 : {data.coment}
-              </Typography>
-            </CardContent>
+                <Typography className={classes.subheading} variant={'caption'}>
+                  성별 : {(data.host.gender = 1 ? '남성' : '여성')}
+                </Typography>
+                <br />
+                <Typography className={classes.subheading} variant={'caption'}>
+                  연락처 : {data.host.phone}
+                </Typography>
+              </CardContent>
+            </div>
           </div>
-          <div className={classes.back}>
-            <CardMedia className={classes.media} component={'img'} image={'https://image.freepik.com/free-photo/river-foggy-mountains-landscape_1204-511.jpg'} />
-            <CardContent className={classes.content}>
-              <Typography className={classes.heading} variant={'h6'} gutterBottom>
-                주최자 : {data.host.name}
-              </Typography>
-              <Typography className={classes.subheading} variant={'caption'}>
-                생년월일 : {data.host.birth}
-              </Typography>
-              <br />
-              <Typography className={classes.subheading} variant={'caption'}>
-                성별 : {(data.host.gender = 1 ? '남성' : '여성')}
-              </Typography>
-              <br />
-              <Typography className={classes.subheading} variant={'caption'}>
-                연락처 : {data.host.phone}
-              </Typography>
-            </CardContent>
-          </div>
-        </div>
-        <CardActions>
-          {button ? (
-            <Button
-              variant="contained"
-              color="primary"
-              style={{ width: '100%', fontSize: '20px' }}
-              onClick={(e) => {
-                handleDelParticipate(e, data.mid, 1)
-                setUpdate(false)
-                didMountRef.current = false
-                setButton(false)
-              }}
-            >
-              취소
-            </Button>
-          ) : (
-            <Button
-              variant="contained"
-              color="primary"
-              style={{ width: '100%', fontSize: '20px' }}
-              onClick={(e) => {
-                handleAddParticipate(e, data.mid, 1)
-                setUpdate(false)
-                didMountRef.current = false
-                setButton(true)
-              }}
-            >
-              참가
-            </Button>
-          )}
-          {/* <Typography style={{ display: 'inline-flex', flex: '1' }}>{data.count}명</Typography> */}
-          <Typography style={{ position: 'absolute', right: '32px', color: 'white' }}>{data.count}명</Typography>
-        </CardActions>
-      </Card>
+          <CardActions>
+            {button ? (
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ width: '100%', fontSize: '20px' }}
+                onClick={(e) => {
+                  handleDelParticipate(e, data.mid, 1)
+                  setUpdate(false)
+                  didMountRef.current = false
+                  setButton(false)
+                }}
+              >
+                취소
+              </Button>
+            ) : (
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ width: '100%', fontSize: '20px' }}
+                onClick={(e) => {
+                  handleAddParticipate(e, data.mid, 1)
+                  setUpdate(false)
+                  didMountRef.current = false
+                  setButton(true)
+                }}
+              >
+                참가
+              </Button>
+            )}
+            {/* <Typography style={{ display: 'inline-flex', flex: '1' }}>{data.count}명</Typography> */}
+            <Typography style={{ position: 'absolute', right: '32px', color: 'white' }}>{data.count}명</Typography>
+          </CardActions>
+        </Card>
+      ) : (
+        <></>
+      )}
       <br />
     </>
   )
