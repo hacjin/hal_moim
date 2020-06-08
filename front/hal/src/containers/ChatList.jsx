97de52ec6f11d375cfe8292cb768f988e673c056
hacjin <macjin102@naver.com>
2020-06-08 20:35:04 +0900

    S02P31A107|front|del_unnecessary

--- front/hal/src/containers/ChatList.jsx
+++ front/hal/src/containers/ChatList.jsx
@@ -1,181 +1,166 @@
-import React from 'react';
-import { ListView } from '@progress/kendo-react-listview';
+import React from 'react'
+import { ListView } from '@progress/kendo-react-listview'
 import API from '../apis/api'
 import ChatItem from '../components/Chat/ChatItem'
-import ChatWindow from '../components/Chat/ChatWindow';
-import incomingMessageSound from '../components/Chat/assets/sounds/notification.mp3';
-import '../styles';
+import ChatWindow from '../components/Chat/ChatWindow'
+import incomingMessageSound from '../components/Chat/assets/sounds/notification.mp3'
+import '../styles'
 import '../styles/all.css'
 import '../styles/bootstrap.min.css'
-import SockJsClient from 'react-stomp';
-
+import SockJsClient from 'react-stomp'
 
 class ChatList extends React.Component {
+  user = JSON.parse(sessionStorage.getItem('user') || '{}')
 
-  user = JSON.parse(sessionStorage.getItem('user') || '{}');
-
-    constructor(props){
-        super(props);
-        this.state = {
-          receiverData:[],
-          isOpen: false,
-          // messageList: [],
-          totalmessageList:{},
-          receiver : '',
-          roomId: '',
-          newMessagesCount: []
-        }
-        this.websocket = React.createRef();
+  constructor(props) {
+    super(props)
+    this.state = {
+      receiverData: [],
+      isOpen: false,
+      // messageList: [],
+      totalmessageList: {},
+      receiver: '',
+      roomId: '',
+      newMessagesCount: [],
     }
-  
-    // from Launcher를 사용하는 Chat.js 에서 가져옴
-    _onMessageWasSent(message) {
-      const chat = {message: "",
-                    type:"",
-                    time: new Date(),
-                    roomId: this.state.roomId,
-                    senderId: this.user.uid};
-      
-      if(message.type === 'text'){
-        chat.message = message.data.text
-        chat.type = 'text'
-      }else if(message.type === 'emoji'){
-        chat.type = 'emoji'
-        chat.message = message.data.emoji
-      }
-      this.websocket.current.sendMessage ('/app/sendMessage/'+this.state.roomId,JSON.stringify(chat));
-      
-    }
+    this.websocket = React.createRef()
+  }
 
-    // Launcher.js 함수
-    componentWillReceiveProps(nextProps) {
-      if (this.props.mute) { return; }
-      const nextMessage = nextProps.totalmessageList[nextProps.totalmessageList.length - 1];
-      const isIncoming = (nextMessage || {}).author === 'them';
-      const isNew = nextProps.totalmessageList.length > this.props.totalmessageList.length;
-      if (isIncoming && isNew) {
-        this.playIncomingMessageSound();
-      }
-    }
-  
-    playIncomingMessageSound() {
-      var audio = new Audio(incomingMessageSound);
-      audio.play();
-    }
-  
-    handleClick() {
-      if (this.props.handleClick !== undefined) {
-        this.props.handleClick();
-      } else {
-        this.setState({
-          isOpen: !this.state.isOpen,
-        });
-      }
-    }
+  // from Launcher를 사용하는 Chat.js 에서 가져옴
+  _onMessageWasSent(message) {
+    const chat = { message: '', type: '', time: new Date(), roomId: this.state.roomId, senderId: this.user.uid }
 
-
-    async componentDidMount() {
-        // Load async data.
-        let userData = await API.get('chat/findRoomListById', {
-          params: {
-            uid: this.user.uid
-          }
-        });
-
-        this.setState({
-          ...this.state, ...{
-            receiverData : userData.data.data,
-          }
-        });
-
+    if (message.type === 'text') {
+      chat.message = message.data.text
+      chat.type = 'text'
+    } else if (message.type === 'emoji') {
+      chat.type = 'emoji'
+      chat.message = message.data.emoji
     }
-    _openChatWindow = (flag,roomId,receiver, totalChatData)=>{
+    this.websocket.current.sendMessage('/app/sendMessage/' + this.state.roomId, JSON.stringify(chat))
+  }
+
+  // Launcher.js 함수
+  componentWillReceiveProps(nextProps) {
+    if (this.props.mute) {
+      return
+    }
+    const nextMessage = nextProps.totalmessageList[nextProps.totalmessageList.length - 1]
+    const isIncoming = (nextMessage || {}).author === 'them'
+    const isNew = nextProps.totalmessageList.length > this.props.totalmessageList.length
+    if (isIncoming && isNew) {
+      this.playIncomingMessageSound()
+    }
+  }
+
+  playIncomingMessageSound() {
+    var audio = new Audio(incomingMessageSound)
+    audio.play()
+  }
+
+  handleClick() {
+    if (this.props.handleClick !== undefined) {
+      this.props.handleClick()
+    } else {
       this.setState({
-        ...this.state, ...{
-          isOpen : flag,
-          receiver : receiver,
-          roomId: roomId,
-          newMessagesCount:{
-            ...this.state.newMessagesCount,
-            [roomId] : 0
-          },
-          totalmessageList: {
-            ...this.state.totalmessageList,
-            [roomId] : totalChatData
-          }
-        }
-      });
-
-    }
-
-
-    MyCustomItem = props => <ChatItem {...props} userId = {this.user.uid} openChatWindow={this._openChatWindow}/>
-    
-    render() {  
-      var topics = []
-      this.state.receiverData.forEach(function(item,index,array) {
-        topics.push('/topic/roomId/'+item.rid)
+        isOpen: !this.state.isOpen,
       })
+    }
+  }
 
+  async componentDidMount() {
+    // Load async data.
+    let userData = await API.get('chat/findRoomListById', {
+      params: {
+        uid: this.user.uid,
+      },
+    })
 
-        return (
-          <div>
-          <SockJsClient 
-          // url={"http://localhost:8080/webSocket" }
-          url={"http://52.78.120.154:8080/webSocket" }
-          topics={topics} 
-          onMessage={msg => { 
-            const newMessagesCount = this.state.isOpen ? this.state.newMessagesCount : this.state.newMessagesCount + 1;
-            var replytext 
-            if(msg.type ==='text'){
-              replytext = {'text':msg.message}
-            }else{
-              replytext = {'emoji':msg.message}
+    this.setState({
+      ...this.state,
+      ...{
+        receiverData: userData.data.data,
+      },
+    })
+  }
+  _openChatWindow = (flag, roomId, receiver, totalChatData) => {
+    this.setState({
+      ...this.state,
+      ...{
+        isOpen: flag,
+        receiver: receiver,
+        roomId: roomId,
+        newMessagesCount: {
+          ...this.state.newMessagesCount,
+          [roomId]: 0,
+        },
+        totalmessageList: {
+          ...this.state.totalmessageList,
+          [roomId]: totalChatData,
+        },
+      },
+    })
+  }
+
+  MyCustomItem = (props) => <ChatItem {...props} userId={this.user.uid} openChatWindow={this._openChatWindow} />
+
+  render() {
+    var topics = []
+    this.state.receiverData.forEach(function (item, index, array) {
+      topics.push('/topic/roomId/' + item.rid)
+    })
+
+    return (
+      <div>
+        <SockJsClient
+          url={'http://52.78.120.154:8080/webSocket'}
+          topics={topics}
+          onMessage={(msg) => {
+            const newMessagesCount = this.state.isOpen ? this.state.newMessagesCount : this.state.newMessagesCount + 1
+            var replytext
+            if (msg.type === 'text') {
+              replytext = { text: msg.message }
+            } else {
+              replytext = { emoji: msg.message }
             }
 
-            var tmpMessageList = this.state.totalmessageList[this.state.roomId] ===undefined ? [] : this.state.totalmessageList[this.state.roomId]
+            var tmpMessageList = this.state.totalmessageList[this.state.roomId] === undefined ? [] : this.state.totalmessageList[this.state.roomId]
             tmpMessageList.push({
-              author: msg.senderId===this.user.uid?'me':'them',
+              author: msg.senderId === this.user.uid ? 'me' : 'them',
               type: msg.type,
-              data: replytext
-              })
+              data: replytext,
+            })
             this.setState({
               ...this.setState,
               newMessagesCount: newMessagesCount,
               totalmessageList: {
                 ...this.state.totalmessageList,
-                [this.state.roomId] : tmpMessageList
-                  
-              }
+                [this.state.roomId]: tmpMessageList,
+              },
             })
           }}
-          ref={this.websocket} /> 
+          ref={this.websocket}
+        />
 
+        <ListView data={this.state.receiverData} item={this.MyCustomItem} style={{ width: '100%' }} />
 
-                <ListView
-                    data={this.state.receiverData}
-                    item={this.MyCustomItem}
-                    style={{ width: "100%" }}
-                />
-                
-                <div id='chat-launcher'>
-                  <ChatWindow
-                    messageList={this.state.totalmessageList[this.state.roomId]}
-                    onUserInputSubmit={this._onMessageWasSent.bind(this)}
-                    onFilesSelected={this.props.onFilesSelected}
-                    agentProfile={{
-                      teamName: this.state.receiver.name,
-                      imageUrl: this.state.receiver.profileImg
-                    }}
-                    isOpen={this.state.isOpen}
-                    onClose={this.handleClick.bind(this)}
-                    showEmoji={true}
-                  />
-                  </div>
-                </div>
-        );
-    }
+        <div id="chat-launcher">
+          <ChatWindow
+            messageList={this.state.totalmessageList[this.state.roomId]}
+            onUserInputSubmit={this._onMessageWasSent.bind(this)}
+            onFilesSelected={this.props.onFilesSelected}
+            agentProfile={{
+              teamName: this.state.receiver.name,
+              imageUrl: this.state.receiver.profileImg,
+            }}
+            isOpen={this.state.isOpen}
+            onClose={this.handleClick.bind(this)}
+            showEmoji={true}
+          />
+        </div>
+      </div>
+    )
+  }
 }
-
 
 export default ChatList
