b5d7c1f238665c647ef4cbf3f1ba3b5ae093821f
hay <hehee.hope@gmail.com>
2020-06-12 16:53:45 +0900

    S02P31A107-78 | front,back | restapi | user api이름 변경, moim back @변경, Moim.tsx정리

--- front/hal/src/components/Mains.jsx
+++ front/hal/src/components/Mains.jsx
@@ -93,10 +93,10 @@ const Mains = (history) => {
             })
             } else {
             // 로그인 페이지(얼굴인식)
-            sessionStorage.setItem('user', JSON.stringify(res.data.data));
+            // sessionStorage.setItem('user', JSON.stringify(res.data.data));
             history.push({
-                // pathname: '/login_face',
-                pathname: '/moim',
+                pathname: '/login_face',
+                // pathname: '/moim',
                 state: { user: res.data.data }
             });
             }
