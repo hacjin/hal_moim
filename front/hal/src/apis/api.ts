3c2a5fdc57b427ea1f57a0198b02093b31e9708d
hacjin <macjin102@naver.com>
2020-06-26 01:13:59 +0900

    modify_for_ssl_on_aws

--- front/hal/src/apis/api.ts
+++ front/hal/src/apis/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://52.78.120.154:8080/api',
+  baseURL: 'https://hacjin.tk:8080/api',
   // baseURL: 'https://localhost:8080/api',
   headers: {},
   // responseType: "json"
 })
 
-export const websocketUri = 'https://52.78.120.154:8080/api/webSocket'
+export const websocketUri = 'https://hacjin.tk:8080/api/webSocket'
